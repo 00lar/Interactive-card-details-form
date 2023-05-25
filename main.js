9f2d6ce6c3811218ef4709675d4e2b4c02419cd5
00lar <eduardani84@gmail.com>
2023-05-25 11:35:24 -0600

    DOM

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const outputCVC = document.querySelector("#outputCVC");
-console.log(CVC)
+const CardHolderOutput = document.querySelector("#CardHolderOutput");
+const Card_NumberOutput = document.querySelector("#Card_NumberOutput");
+const MonthOutput = document.querySelector("#MonthOutput");
+const YearOutput = document.querySelector("#YearOutput");
+console.log(CVC);
 
 function ActualizarValor(){
     const CVC = document.querySelector("#CVC").value;
     outputCVC.textContent = CVC;
+
+    const Cardholder_name = document.querySelector("#Cardholder_name").value;
+    CardHolderOutput.textContent = Cardholder_name;
+
+     const Card_Number = document.querySelector("#Card_Number").value;
+     Card_NumberOutput.textContent = Card_Number;
+
+     const Month = document.querySelector("#Month").value;
+     MonthOutput.textContent = Month;
+
+     const Year = document.querySelector("#Year").value;
+     YearOutput.textContent = Year;
 }
