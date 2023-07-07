1e079d4dc8b3b82def657bf2c9ca60728c441044
00lar <eduardani84@gmail.com>
2023-07-07 17:09:39 -0600

    ajustes para regresar

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ const MonthOutput = document.querySelector("#MonthOutput");
 const YearOutput = document.querySelector("#YearOutput");
 console.log(CVC);
 
+inputCVC.add
+
 function ActualizarValor(){
-    const CVC = document.querySelector("#CVC").value;
+    let CVC = document.querySelector("#CVC").value;
+    cvc_lengt = CVC.length;
     outputCVC.textContent = CVC;
+    if (cvc_lengt > 3) {
+       
+        CVC = CVC.slice(0,3);
+        console.log(CVC)
+     }  
+
 
     const Cardholder_name = document.querySelector("#Cardholder_name").value;
     CardHolderOutput.textContent = Cardholder_name;
 
      const Card_Number = document.querySelector("#Card_Number").value;
      Card_NumberOutput.textContent = Card_Number;
+
 
      const Month = document.querySelector("#Month").value;
      MonthOutput.textContent = Month;
